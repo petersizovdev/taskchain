5964a7e59401276777957b760ba0724fe07f5b96
pitersizovdev <pitersizov@gmail.com>
2024-05-31 21:29:57 +0300

    0.16.0

--- src/app/components/Header/Header.js
+++ src/app/components/Header/Header.js
@@ -1,30 +1,67 @@
+"use client";
+import React, { useState } from "react";
 import styles from "./header.module.css";
 import lm from "@/assets/svg/logomobile.svg";
 import lt from "@/assets/svg/logotext.svg";
 import Image from "next/image";
 import Button from "../Button/Button";
 import Link from "next/link";
+import { FiMoreHorizontal, FiX } from "react-icons/fi";
 
 const Header = () => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <div className={styles.header}>
       <div className={styles.logo}>
         <Link href="/">
-          <Image className={styles.logoMobile} src={lm}></Image>
-          <Image className={styles.logoText} src={lt}></Image>
+          <Image className={styles.logoMobile} src={lm} alt="logo" />
+          <Image className={styles.logoText} src={lt} alt="logo text" />
         </Link>
       </div>
-      <div className={styles.nav}>
+      <div className={`${styles.nav} ${isSidebarOpen ? styles.hide : ""}`}>
         <Link href="#faq">
-          <Button className={"stock"}>О нас</Button>
+          <Button className="stock">О нас</Button>
         </Link>
         <Link href="/feedback">
-          <Button className={"stock"}>Контакты</Button>
+          <Button className="stock">Контакты</Button>
         </Link>
         <Link href="/messenger">
-          <Button className={"accent"}> Мессенджер</Button>
+          <Button className="accent">Мессенджер</Button>
         </Link>
       </div>
+
+      {isSidebarOpen && (
+        <div className={`${styles.SideBarMenuContainer} ${styles.show}`}>
+          <div className={styles.sideBarMenuNav}>
+            <Button className="stock" onClick={toggleSidebar}>
+              {isSidebarOpen ? <FiX /> : <FiMoreHorizontal />}
+            </Button>
+
+            <Link href="#faq">
+              <Button className="stock">О нас</Button>
+            </Link>
+            <Link href="/feedback">
+              <Button className="stock">Контакты</Button>
+            </Link>
+            <Link href="/messenger">
+              <Button className="accent">Мессенджер</Button>
+            </Link>
+          </div>
+        </div>
+      )}
+
+      {!isSidebarOpen && (
+        <div className={styles.toggleMenu}>
+          <Button className="stock" onClick={toggleSidebar}>
+            <FiMoreHorizontal />
+          </Button>{" "}
+        </div>
+      )}
     </div>
   );
 };
--- src/app/components/ToggleMenu/ToggleMenu.js
+++ src/app/components/ToggleMenu/ToggleMenu.js
@@ -1,14 +1,10 @@
 'use client'
-
 import React, { useState } from "react";
 import Button from "../Button/Button";
-import Chats from "../Chats/Chats";
-import Search from "../Search/Search";
-import SideBarNav from "../SideBarNav/SideBarNav";
 import { FiMoreHorizontal, FiX } from "react-icons/fi";
-import styles from "./sidebar.module.scss";
+import styles from "./togglemenu.module.scss";
 
-const SideBarMenu = () => {
+const ToggleMenu = () => {
   const [isSidebarOpen, setSidebarOpen] = useState(true);
 
   const toggleSidebar = () => {
@@ -29,7 +25,6 @@ const SideBarMenu = () => {
         }}
       >
         <div className={styles.sideBarMenuNav}>
-          <SideBarNav />
           <div className={styles.sideBarMenuNavButton}>
             <Button className={"stock"} onClick={toggleSidebar}>
               <FiX />
@@ -37,13 +32,9 @@ const SideBarMenu = () => {
           </div>
         </div>
 
-        <Search />
-        <div className={styles.sideBarMenuChat} onClick={toggleSidebar}>
-          <Chats />
-        </div>
       </div>
     </>
   );
 };
 
-export default SideBarMenu;
+export default ToggleMenu;
